f789d8190af029395b2bcdf32497eed162e4f56c
SDinesh-25 <dk.sridinesh@gmail.com>
2024-07-29 11:26:44 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Loginform from "./Components/Loginform/Loginform.jsx"
+import './App.css'
+
+function App() {
+
+ return(
+  <div className="App">
+    <Loginform />
+       
+  </div>
+ )
+}
+
+export default App
